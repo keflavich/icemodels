9a723bbe00731c121526cb9412b9bc36c4fa394f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2023-01-04 18:44:19 -0500

    refactoring

--- icemodels/__init__.py
+++ icemodels/__init__.py
@@ -7,7 +7,9 @@ from ._astropy_init import *   # noqa
 # ----------------------------------------------------------------------------
 
 from .core import (absorbed_spectrum, absorbed_spectrum_Gaussians, convsum,
-                   fluxes_in_filters, load_molecule)
+                   fluxes_in_filters, load_molecule, atmo_model, molecule_data)
+import gaussian_model_components
 
 __all__ = [absorbed_spectrum, absorbed_spectrum_Gaussians, convsum,
-           fluxes_in_filters, load_molecule]
+           fluxes_in_filters, load_molecule, atmo_model, molecule_data,
+           gaussian_model_components]
---
+++ icemodels/gaussian_model_components.py
@@ -0,0 +1,36 @@
+from astropy import units as u
+
+# apolar in CO2, apolar pure CO, polar methanol ice
+co_ice_wls_icm = ([2143.7, 2139.9, 2136.5] * u.cm**-1)
+co_ice_centers = co_ice_wls = co_ice_wls_icm.to(u.um, u.spectral())
+co_ice_widths = (([3.0, 3.5, 10.6] * u.cm**-1)/co_ice_wls_icm * co_ice_wls).to(u.um, u.spectral())
+co_ice_bandstrength = 1.1e-17 * u.cm # cm per molecule; Jian+1975 via Boogert+2022
+co_ice_bandstrengths = [co_ice_bandstrength/3]*3
+co_ice_wls, co_ice_widths
+
+ocn_center = 4.62*u.um
+ocn_width = 100*u.cm**-1 / (ocn_center.to(u.cm**-1, u.spectral())) * ocn_center
+ocs_center = 4.90*u.um
+ocn_bandstrength = 1.3e-16*u.cm # per molecule; via Boogert+2022 from van Broekhuizen+2004
+ocs_bandstrength = 1.5e-16*u.cm # per molecule; via Boogert+2022 from Palumbo+1997
+
+water_ice_centers_icm = [1666, 3333]*u.cm**-1
+water_ice_widths_icm = [160, 390]*u.cm**-1
+water_ice_centers = water_ice_centers_icm.to(u.um, u.spectral())
+water_ice_widths = (water_ice_widths_icm / water_ice_centers_icm) * water_ice_centers
+water_ice_bandstrengths = [1.2e-17, 2e-16]*u.cm
+# first is Gerakines+1995 via Boogert+2007
+# second Hagen+1981 vis Boogert+2022 - but this is for the 3um band, not the 6um?
+nh4_ice_centers = []
+nh4_ice_widths = []
+nh4_ice_bandstrengths = []
+
+# Hudgins 1993, with centers & widths from Boogert 2015
+# these don't always agree well
+# some also come from Allamandola 1992 (2832 from their 2825 in tbl 2, 2597 from footnote of tbl 3)
+methanol_ice_centers_icm = [2881.8, 2832, 2597, 2538, 1459, 1128, 1026]*u.cm**-1
+methanol_ice_widths_icm = [80, 30, 40, 40, 85, 15, 30]*u.cm**-1
+methanol_ice_centers = methanol_ice_centers_icm.to(u.um, u.spectral())
+methanol_ice_widths = (methanol_ice_widths_icm / methanol_ice_centers_icm) * methanol_ice_centers
+methanol_ice_bandstrengths = [5.6e-18, 4e-18, 2.6e-18, 2.8e-18, 1.2e-17, 1.8e-18, 1.8e-17]*u.cm
+methanol_ice_centers_icm.to(u.um, u.spectral())
