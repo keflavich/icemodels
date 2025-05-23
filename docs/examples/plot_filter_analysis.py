1625a125ca4461be3559ef2e9c19a35c4b085835
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2025-05-22 23:19:53 -0400

    fixed one weird error

--- docs/examples/plot_filter_analysis.py
+++ docs/examples/plot_filter_analysis.py
@@ -10,6 +10,7 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import interp1d
+from astroquery.svo_fps import SvoFps
 
 # Create a common wavelength grid
 wavelength = np.linspace(1, 28, 1000) * u.um
@@ -39,13 +40,17 @@ plt.title('CO2 Ice Spectrum with JWST/MIRI Filters')
 plt.legend()
 
 # Calculate and print filter fluxes
-filter_ids = ['JWST/MIRI.F1000W', 'JWST/MIRI.F1280W']
+filter_ids = ['JWST/MIRI.F1000W', 'JWST/MIRI.F1280W']  # Full SVO FPS IDs
 filter_fluxes = {}
+
+transdata = {fid: SvoFps.get_transmission_data(fid) for fid in filter_ids}
+
 for filter_id in filter_ids:
     flux = icemodels.fluxes_in_filters(
         xarr=wavelength,
         modeldata=spectrum,
-        filterids=[filter_id]
+        filterids=[filter_id],
+        transdata=transdata,
     )
     filter_fluxes[filter_id] = flux
     print(f"Flux through {filter_id}: {flux}")
